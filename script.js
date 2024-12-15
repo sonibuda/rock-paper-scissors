dd0371f6c5209c83e7b639307404feb74ce1e7c9
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-14 19:44:46 -0700

    Add function to play 5 round game.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //rock = 1, paper 2, scissors 3
-var humanScore = 0; //declares a variable to track the player's score and initializes it to 0.
-var computerScore = 0; //declares a variable to track computer score and initializes it to 0.
+
 
  //get Computer Choice multiplies random number between 0 and 0.99 by 3, then
 // rounds it down to get a whole number between 1 and 3.
+
+alert("Let's play! Best of 5.")
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() *3) +1;
     switch (computerChoice) {
@@ -22,48 +24,72 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanScore, computerScore) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
     switch (humanSelection + computerSelection) {
         case "rockrock":
-            console.log("Computer chose rock. It's a tie.");
+            alert("Computer chose rock. It's a tie.");
             break;
         case "rockscissors":
-            console.log("Rock crushes scissors. You win this round!");
+            alert("Rock crushes scissors. You win this round!");
             humanScore++;
             break;
         case "rockpaper":
-            console.log("Paper covers rock. Computer wins this round.");
+            alert("Paper covers rock. Computer wins this round.");
             computerScore++;
             break;
         case "scissorsrock":
-            console.log("Scissors get crushed by the computer's rock. Better luck next time.");
+            alert("Scissors get crushed by the computer's rock. Better luck next time.");
             computerScore++;
             break;
         case "scissorsscissors":
-            console.log("Computer also chose scissors. You tied.");
+            alert("Computer also chose scissors. You tied.");
             break;
         case "scissorspaper":
-            console.log("Scissors slices paper. You win this round!");
+            alert("Scissors slices paper. You win this round!");
             humanScore++;
             break;
         case "paperrock":
-            console.log("Paper covers rock. You win this round!");
+            alert("Paper covers rock. You win this round!");
             humanScore++;
             break;
         case "paperscissors":
-            console.log("The computer's scissors chopped up your paper. 2nd place isn't too bad.");
+            alert("The computer's scissors chopped up your paper. 2nd place isn't too bad.");
             computerScore++;
             break;
         case "paperpaper":
-            console.log("You both chose paper. It's a tie.");
+            alert("You both chose paper. It's a tie.");
             break;
 
     }
+    return [humanScore, computerScore];
 }
 
-playRound();
-console.log(humanScore);
-console.log(computerScore);
+
+
+function playGame() {
+    let humanScore = 0; //declares a variable to track the player's score and initializes it to 0.
+    let computerScore = 0; //declares a variable to track computer score and initializes it to 0.
+[humanScore, computerScore] = playRound(humanScore, computerScore);
+alert("Your score for round 1 is " + humanScore + " The computer's score is " + computerScore);
+[humanScore, computerScore] = playRound(humanScore, computerScore);
+alert("Your score for round 2 is " + humanScore + " The computer's score is " + computerScore);
+[humanScore, computerScore] = playRound(humanScore, computerScore);
+alert("Your score for round 3 is " + humanScore + " The computer's score is " + computerScore);
+[humanScore, computerScore] = playRound(humanScore, computerScore);
+alert("Your score for round 4 is " + humanScore + " The computer's score is " + computerScore);
+[humanScore, computerScore] = playRound(humanScore, computerScore);
+alert("Your score for round 5 is " + humanScore + " The computer's score is " + computerScore);
+if (humanScore > computerScore) {
+    alert("You win!");
+} else if (humanScore < computerScore) {
+    alert("Computer wins.");
+} else {
+    alert("It's a tie.");
+}
+}
+
+
+playGame();
